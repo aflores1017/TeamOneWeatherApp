4fcfc7aceebfebe929a55e5a40def97414b65c60
Starsii <106503875+Starsii@users.noreply.github.com>
2022-12-12 15:01:36 -0600

    updates

--- WeatherWebApp/myWeatherApp/functions.js
+++ WeatherWebApp/myWeatherApp/functions.js
@@ -6,20 +6,22 @@ const sunnyDay = "https://th.bing.com/th/id/R.722f1fe1ea26153501740d3ea546ac84?r
 const weatherAPI = "https://api.open-meteo.com/v1/forecast?latitude=41.85003&longitude=-87.65005&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m"
 
 function changeImage(){
-    const element= document.getElementById('temp_input');
-    const elementValue = element.value;
+    let image = document.getElementById('images');
+    // const element= document.getElementById('temp_input');
+    // const elementValue = element.value;
     fetch(weatherAPI).then((response)=>{
         response.json().then((data)=>{
             //console.log(data.current_weather.temperature);
-            const targetDiv = document.getElementById('Weather-Images')
-            targetDiv.innerHTML = JSON.stringify(data.current_weather.temperature);
+            //const targetDiv = document.getElementById('Weather-Images')
+            //targetDiv.innerHTML = JSON.stringify(data.current_weather.temperature);
 
             switch (true) {
               case (3.4 > 10): 
                 console.log("this is very tiring");
                 break;
               case (3.4 < 10):
-                targetDiv.innerHTML = snowDay;  
+                image.src = rainyDay;  
+                console.log("nice");
                 break;
               default: 
                 console.log("this is the default");
